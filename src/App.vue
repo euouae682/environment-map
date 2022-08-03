1c2c12d130d0c1c176d8d4b5996518d6c8612b8a
euouae682 <jasonuzhao682@gmail.com>
2022-08-02 17:43:18 -0700

    Created template navbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="Hello World!" />
+      <Header name="Environment Map" />
     </div>
   </header>
 </template>
 
 <style scoped>
-.wrapper {
-  margin: auto;
-  text-align: center;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <header>
+    <h1>{{ name }}</h1>
+    <ul>
+        <li><a>About</a></li>
+        <l1><a>Additional Sources</a></l1>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+header {
+    /* background-color: rgb(12, 139, 0); */
+    /* color: rgb(240, 240, 240); */
+    height: 5em;
+    line-height: 5em;
+    border: 3px solid black;
+}
+
+h1 {
+    display: inline;
+    padding-left: 0.75em;
+}
+
+ul {
+    float: right;
+    list-style-type: none;
+    /* border: 1px solid red; */
+}
+
+li {
+    float: left;
+    text-align: center;
+}
+
+a {
+    padding: 25px;
+    font-size: 20px;
+}
+
+a:hover {
+    background-color: rgb(231, 231, 231);
+    cursor: pointer;
+    
+}
+</style>
