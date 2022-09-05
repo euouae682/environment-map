212362734e78757c73179a33a61b8d3c8245e521
euouae682 <jasonuzhao682@gmail.com>
2022-09-04 18:03:50 -0700

    added da svg into da page

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -9,7 +9,8 @@ import { store } from '../store.js';
     <!-- PLACEHOLDER IMAGE -->
     <!-- <img v-if="store.viewMode" src="../assets/darkmap.PNG">
     <img v-else src="../assets/lightmap.PNG"> -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d59282557.89881275!2d-22.324166368248864!3d24.921160135292435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1660091421742!5m2!1sen!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d59282557.89881275!2d-22.324166368248864!3d24.921160135292435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1660091421742!5m2!1sen!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
+    <img src="../assets/BlankMap-World.svg">
   </div>
 </template>
 
@@ -20,7 +21,7 @@ import { store } from '../store.js';
     overflow: hidden;
 }
 
-iframe {
+img, iframe {
   width: 100%;
   height: 100%;
 }
