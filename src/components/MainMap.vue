db536f0664b593f514711989da75cf4ede01a9da
euouae682 <jasonuzhao682@gmail.com>
2022-09-05 21:56:43 -0700

    inserted world map and hover highlights

--- src/components/MainMap.vue
+++ src/components/MainMap.vue
@@ -1,6 +1,7 @@
 <script setup>
 /* Import state "store" */
 import { store } from '../store.js';
+import WorldMap from '../assets/WorldMap.vue';
 </script>
 
 <template>
@@ -10,19 +11,21 @@ import { store } from '../store.js';
     <!-- <img v-if="store.viewMode" src="../assets/darkmap.PNG">
     <img v-else src="../assets/lightmap.PNG"> -->
     <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d59282557.89881275!2d-22.324166368248864!3d24.921160135292435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1660091421742!5m2!1sen!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
-    <img src="../assets/BlankMap-World.svg">
+    <!-- <img src="../assets/BlankMap-World.svg"> -->
+    <WorldMap />
+    <a href="https://commons.wikimedia.org/wiki/File:BlankMap-World.svg">Canuckguy (talk) and many others (see File history)</a>, Public domain, via Wikimedia Commons
   </div>
 </template>
 
 <style scoped>
 .map {
     width: 100%;
-    height: 100%;
-    overflow: hidden;
+    /* height: 100%; */
+    overflow: scroll;
 }
 
-img, iframe {
+/* img, iframe {
   width: 100%;
   height: 100%;
-}
+} */
 </style>
